eb6da172c05b412382cdec838e0c88cc6fca840e
Fábio <fabio.dev.gomes@gmail.com>
2017-01-04 10:55:15 -0200

    Configuraçao de acesso a banco

--- src/main/java/br/com/casadocodigo/loja/managedbeans/AdminBooksBean.java
+++ src/main/java/br/com/casadocodigo/loja/managedbeans/AdminBooksBean.java
@@ -1,25 +1,27 @@
 package br.com.casadocodigo.loja.managedbeans;
 
 import javax.enterprise.inject.Model;
+import javax.inject.Inject;
 
+import br.com.casadocodigo.loja.daos.BookDao;
 import br.com.casadocodigo.loja.models.Book;
 
 @Model
 public class AdminBooksBean {
 	
 	private Book product = new Book();
+	@Inject
+	private BookDao bookDao;
 
 	public void save(){
 		System.out.println("-- save() --" + product);
+		bookDao.save(product);
 	}
 
 	public Book getProduct() {
 		return product;
 	}
-//
-//	public void setProduct(Book product) {
-//		this.product = product;
-//	}
-//	
+	
+	
 	
 }
