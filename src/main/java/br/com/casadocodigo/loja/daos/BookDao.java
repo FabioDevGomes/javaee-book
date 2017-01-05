36bc784eec01fc06b98b2d303b514342432e4d21
Fábio <fabio.dev.gomes@gmail.com>
2017-01-05 11:14:23 -0200

    Banco de dados configurado

--- src/main/java/br/com/casadocodigo/loja/daos/BookDao.java
+++ src/main/java/br/com/casadocodigo/loja/daos/BookDao.java
@@ -2,6 +2,7 @@ package br.com.casadocodigo.loja.daos;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 import br.com.casadocodigo.loja.models.Book;
 
@@ -10,6 +11,7 @@ public class BookDao {
 	@PersistenceContext
 	private EntityManager entityManager;
 	
+	@Transactional
 	public void save(Book book){
 		entityManager.persist(book);
 	}
