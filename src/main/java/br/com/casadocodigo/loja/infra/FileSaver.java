54e6612ab6d26211cba6a29f68a0b2d2c13bfcac
Fábio <fabio.dev.gomes@gmail.com>
2017-05-05 18:44:07 -0300

    Extração de código para melhorar coesão

--- src/main/java/br/com/casadocodigo/loja/infra/FileSaver.java
+++ src/main/java/br/com/casadocodigo/loja/infra/FileSaver.java
@@ -1,29 +1,23 @@
 package br.com.casadocodigo.loja.infra;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.servlet.http.Part;
 
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.S3ClientOptions;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
 @RequestScoped
 public class FileSaver {
 
-	//	@Inject
-	//	private HttpServletRequest request;
-//	@Inject
-//	private AmazonS3Client s3;
+	@Inject
+	private AmazonS3Client s3;
 	private static final String CONTENT_DISPOSITION = "content-disposition";
 
 	public String writer(String baseFolder, Part multpartFile) {
 		//String serverPath = request.getServletContext().getRealPath("/" + baseFolder);
-		// String path = serverPath + "/" + fileName;
+		//String path = serverPath + "/" + fileName;
 
-		AmazonS3Client s3 = client();
 		String fileName = extractFileName(multpartFile.getHeader(CONTENT_DISPOSITION));
 
 		try {
@@ -61,18 +55,6 @@ public class FileSaver {
 		}
 		return fileName;
 	}
-
-	private AmazonS3Client client() {
-		AWSCredentials awsCredentials = new BasicAWSCredentials("AKIAIOSFODNN7EXAMPLE",
-				"wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY");
-
-		AmazonS3Client newClient = new AmazonS3Client(awsCredentials,
-				new ClientConfiguration());
-
-		newClient.setS3ClientOptions(new S3ClientOptions().withPathStyleAccess(true));
-		newClient.setEndpoint("http://localhost:9444/s3");
-
-		return newClient;
-	}
+	
 
 }
