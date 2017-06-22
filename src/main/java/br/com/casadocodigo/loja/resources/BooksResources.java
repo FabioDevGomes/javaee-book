8d79369378b97a4ffce42f3b38e5800317f46d4d
Fábio <fabio.dev.gomes@gmail.com>
2017-06-22 14:30:22 -0300

    Negociação de Conteúdo

--- src/main/java/br/com/casadocodigo/loja/resources/BooksResources.java
+++ src/main/java/br/com/casadocodigo/loja/resources/BooksResources.java
@@ -2,25 +2,44 @@ package br.com.casadocodigo.loja.resources;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateful;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jboss.resteasy.annotations.providers.jaxb.Wrapped;
+
 import br.com.casadocodigo.loja.daos.BookDao;
 import br.com.casadocodigo.loja.models.Book;
 
 @Path("books")
+@Stateful
+@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 public class BooksResources {
 
-	@Inject
 	private BookDao bookDao;
+	@PersistenceContext(type=PersistenceContextType.EXTENDED)
+	private EntityManager entityManager;
+
+	@PostConstruct
+	private void LoadDAO(){
+		bookDao = new BookDao(entityManager);
+	}
 	
 	@GET
-	@Produces({MediaType.APPLICATION_JSON})
-	@Path("json")
 	public List<Book> lastBookJson(){
+		return bookDao.lastReleases();
+	}
+
+	@GET
+	@Wrapped(element="books")
+	public List<Book> lastBookXml(){
 		return bookDao.lastReleases();
 	}
 	
