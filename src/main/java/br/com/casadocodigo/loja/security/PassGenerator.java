1439ad9ac0a03d185464db4050ff0b8589709fcd
Fábio <fabio.dev.gomes@gmail.com>
2017-07-11 14:30:03 -0300

    Segurança com JAAS

--- src/main/java/br/com/casadocodigo/loja/security/PassGenerator.java
+++ src/main/java/br/com/casadocodigo/loja/security/PassGenerator.java
@@ -5,7 +5,7 @@ import org.jboss.security.Base64Encoder;
 public class PassGenerator {
 
 	public static void main(String[] args) throws Exception {
-		Base64Encoder.main(new String[]{"12345", "SHA"});
+		Base64Encoder.main(new String[]{"12345", "SHA-256"});
 	}
 	
 }
